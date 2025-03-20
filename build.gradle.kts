0439ce88b8040fde01f3ffe0b52f4e06a21a9a47
Claus Polanka <claus.polanka@chello.at>
2025-03-20 22:15:32 +0100

    Initialise project

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    idea
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+repositories {
+    mavenCentral()
+}
