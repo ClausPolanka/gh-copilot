bf28443467762e999645f50d0733d94ab3f66843
Claus Polanka <claus.polanka@chello.at>
2025-03-21 00:05:39 +0100

    Support counting latin alphabetic characters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,1 +1,27 @@
-fun main() = println("Hello World")
+fun main() {
+    print("Please enter text: ")
+    val userInput = readUserInput() ?: return
+    val wordCount = countLatinAlphabeticWords(userInput)
+    println("The text contains $wordCount word(s).")
+}
+
+fun readUserInput(): String? {
+    return readlnOrNull() ?: run {
+        println()
+        println("Something went wrong. Please try again.")
+        null
+    }
+}
+
+fun countLatinAlphabeticWords(userInput: String): Int {
+    return userInput
+        .takeUnless { it.isBlank() }
+        ?.split("\\s+".toRegex())
+        ?.filter(String::isNotBlank)
+        ?.count(::isLatinAlphabeticWord)
+        ?: 0
+}
+
+fun isLatinAlphabeticWord(word: String): Boolean {
+    return word.all { it.isLetter() }
+}
