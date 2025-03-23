6a1306dd9dc95c59c4717fb9eb5f1c5bc0675ee9
Claus Polanka <claus.polanka@chello.at>
2025-03-23 12:55:04 +0100

    Add unit tests for analyser

---
+++ src/test/kotlin/ut/wordcounter/domain/WhiteSpacesSeparatedWordsAnalyserTest.kt
@@ -0,0 +1,49 @@
+package ut.wordcounter.domain
+
+import WhiteSpacesSeparatedWordsAnalyser
+import WordsListener
+import kotlin.test.*
+
+class WhiteSpacesSeparatedWordsAnalyserTest {
+    @Test
+    fun `analyses text containing two words separated by one white space`() {
+        var actual: List<String> = emptyList()
+        val sut = WhiteSpacesSeparatedWordsAnalyser(
+            wordsListener = object : WordsListener {
+                override fun onWordsAnalysed(words: List<String>) {
+                    actual = words
+                }
+            }
+        )
+        sut.analyse("word word")
+        assertEquals(listOf("word", "word"), actual, "words")
+    }
+
+    @Test
+    fun `analyses empty text`() {
+        var actual: List<String> = emptyList()
+        val sut = WhiteSpacesSeparatedWordsAnalyser(
+            wordsListener = object : WordsListener {
+                override fun onWordsAnalysed(words: List<String>) {
+                    actual = words
+                }
+            }
+        )
+        sut.analyse(text = "")
+        assertEquals(emptyList(), actual, "words")
+    }
+
+    @Test
+    fun `analyses blank text`() {
+        var actual: List<String> = emptyList()
+        val sut = WhiteSpacesSeparatedWordsAnalyser(
+            wordsListener = object : WordsListener {
+                override fun onWordsAnalysed(words: List<String>) {
+                    actual = words
+                }
+            }
+        )
+        sut.analyse(text = "    ")
+        assertEquals(emptyList(), actual, "words")
+    }
+}
