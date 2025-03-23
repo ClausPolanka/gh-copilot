ebe97d5e678518c9793f3d12f671c9228304c4ab
Claus Polanka <claus.polanka@chello.at>
2025-03-23 14:08:09 +0100

    Add static import

--- src/test/kotlin/it/wordcounter/EndToEndTests.kt
+++ src/test/kotlin/it/wordcounter/EndToEndTests.kt
@@ -1,7 +1,8 @@
 package it.wordcounter
 
 import main
-import org.hamcrest.*
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.*
 import org.junit.*
 import java.io.*
 
@@ -10,59 +11,41 @@ class EndToEndTests {
     fun `a user enters text containing latin alphabetic words while ignoring stop words`() {
         aUserEnters("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.")
         main()
-        MatcherAssert.assertThat(
-            uiOutput(),
-            CoreMatchers.containsString("The text contains 9 word(s), 7 of them unique.")
-        )
+        assertThat(uiOutput(), containsString("The text contains 9 word(s), 7 of them unique."))
     }
 
     @Test
     fun `a user provides a file as a user input source`() {
         main(arrayOf("my_text.txt"))
-        MatcherAssert.assertThat(
-            uiOutput(),
-            CoreMatchers.containsString("The text contains 4 word(s), 4 of them unique.")
-        )
+        assertThat(uiOutput(), containsString("The text contains 4 word(s), 4 of them unique."))
     }
 
     @Test
     fun `a user enters a word containing numbers`() {
         aUserEnters("Ma3ry")
         main()
-        MatcherAssert.assertThat(
-            uiOutput(),
-            CoreMatchers.containsString("The text contains 0 word(s), 0 of them unique.")
-        )
+        assertThat(uiOutput(), containsString("The text contains 0 word(s), 0 of them unique."))
     }
 
     @Test
     fun `a user enters a word ending with a punctuation mark`() {
         aUserEnters("Mary?")
         main()
-        MatcherAssert.assertThat(
-            uiOutput(),
-            CoreMatchers.containsString("The text contains 1 word(s), 1 of them unique.")
-        )
+        assertThat(uiOutput(), containsString("The text contains 1 word(s), 1 of them unique."))
     }
 
     @Test
     fun `a user enters a blank text`() {
         aUserEnters("    ")
         main()
-        MatcherAssert.assertThat(
-            uiOutput(),
-            CoreMatchers.containsString("The text contains 0 word(s), 0 of them unique.")
-        )
+        assertThat(uiOutput(), containsString("The text contains 0 word(s), 0 of them unique."))
     }
 
     @Test
     fun `a user enters nothing and then presses enter`() {
         aUserEnters("")
         main()
-        MatcherAssert.assertThat(
-            uiOutput(),
-            CoreMatchers.containsString("The text contains 0 word(s), 0 of them unique.")
-        )
+        assertThat(uiOutput(), containsString("The text contains 0 word(s), 0 of them unique."))
     }
 
     private fun uiOutput() = outputStream.toString().trim()
