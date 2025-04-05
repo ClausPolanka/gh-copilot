4f2007604c2794fb12e20bfff90117720ea2c466
Claus Polanka <claus.polanka@chello.at>
2025-04-05 19:32:33 +0200

    Use with to reduce duplication

--- src/main/kotlin/wordcounter/io/presentation/ConsoleWordCountListener.kt
+++ src/main/kotlin/wordcounter/io/presentation/ConsoleWordCountListener.kt
@@ -5,9 +5,11 @@ import wordcounter.domain.counting.*
 class ConsoleWordCountListener : WordCountListener {
     override fun onWordsCounted(wordCount: WordCount) {
         val output = buildString {
-            append("The text contains ${wordCount.regularWordCount} word(s), ")
-            append("${wordCount.uniqueWordCount} of them unique. ")
-            append("The average word length is ${"%.2f".format(wordCount.averageWordLength)} characters long.")
+            with(wordCount) {
+                append("The text contains $regularWordCount word(s), ")
+                append("$uniqueWordCount of them unique. ")
+                append("The average word length is ${"%.2f".format(averageWordLength)} characters long.")
+            }
         }
         println(output)
     }
