2c9a126deb8603cd5359a33e14d44dfee7824154
Claus Polanka <claus.polanka@chello.at>
2025-04-08 21:50:09 +0200

    Rename variables to be more generic

--- src/main/kotlin/wordcounter/Main.kt
+++ src/main/kotlin/wordcounter/Main.kt
@@ -32,10 +32,10 @@ fun main(args: Array<String> = emptyArray()) {
 }
 
 fun LatinAlphabeticWordCounter(wordCountListener: WordCountListener): WordCounter {
-    val latinAlphabeticWordsFilter: (String) -> Boolean = { w -> w.all { c -> c.isLetter() } }
-    val nonEmptyWordsFilter: (String) -> Boolean = { it.isNotEmpty() }
+    val latinAlphabeticStringFilter: (String) -> Boolean = { s -> s.all { c -> c.isLetter() } }
+    val nonEmptyStringFilter: (String) -> Boolean = { it.isNotEmpty() }
     return WordCounter(
-        wordsFilter = listOf(nonEmptyWordsFilter, latinAlphabeticWordsFilter),
+        wordsFilter = listOf(nonEmptyStringFilter, latinAlphabeticStringFilter),
         wordCountListener = wordCountListener
     )
 }
