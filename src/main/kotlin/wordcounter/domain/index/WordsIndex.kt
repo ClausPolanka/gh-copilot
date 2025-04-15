ddfa05e7ed764759bca6d11b101e99a9688110c0
Claus Polanka <claus.polanka@chello.at>
2025-04-15 21:44:41 +0200

    Convert to expression body

--- src/main/kotlin/wordcounter/domain/index/WordsIndex.kt
+++ src/main/kotlin/wordcounter/domain/index/WordsIndex.kt
@@ -6,9 +6,9 @@ import wordcounter.domain.words.*
 class WordsIndex(
     private val wordsIndexListener: WordsIndexListener,
 ) : WordsListener {
-    override fun onWordsAnalysed(words: List<String>) {
+    override fun onWordsAnalysed(words: List<String>) =
         wordsIndexListener.onWordsIndexed(words.index())
-    }
 
-    private fun List<String>.index(): List<String> = sortedBy { it.lowercase() }.distinct()
+    private fun List<String>.index(): List<String> =
+        sortedBy { it.lowercase() }.distinct()
 }
