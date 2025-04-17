a86a9d5f6ad30f5a98935f2b870b4d3a9f3333a8
Claus Polanka <claus.polanka@chello.at>
2025-04-17 21:28:25 +0200

    Rename method to reflect class name

--- src/main/kotlin/wordcounter/domain/index/impl/WordsIndexCheckedAgainstDictionary.kt
+++ src/main/kotlin/wordcounter/domain/index/impl/WordsIndexCheckedAgainstDictionary.kt
@@ -9,10 +9,10 @@ class WordsIndexCheckedAgainstDictionary(
     private val dictionary: List<String>,
 ) : WordsIndexListener {
     override fun onWordsIndexed(words: List<String>) {
-        val index = words.indexUsing(dictionary)
+        val index = words.indexCheckedAgainst(dictionary)
         wordsIndexListener.onWordsIndexed(index)
     }
 
-    private fun List<String>.indexUsing(dictionary: List<String>): List<String> =
+    private fun List<String>.indexCheckedAgainst(dictionary: List<String>): List<String> =
         map { word -> word.takeIf(dictionary::contains) ?: (word + UNKNOWN_MARK) }
 }
