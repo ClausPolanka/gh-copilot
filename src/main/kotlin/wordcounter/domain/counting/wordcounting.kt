8c870a2df0cb67e44c8b440f4a21d54f81fbe271
Claus Polanka <claus.polanka@chello.at>
2025-04-17 22:56:14 +0200

    Extract concept to contain words filter

--- src/main/kotlin/wordcounter/domain/counting/wordcounting.kt
+++ src/main/kotlin/wordcounter/domain/counting/wordcounting.kt
@@ -1,10 +1,11 @@
 package wordcounter.domain.counting
 
+import wordcounter.domain.wordsfiltering.*
+
 fun LatinAlphabeticWordCounter(wordCountListener: WordCountListener): WordCounter {
-    val latinAlphabeticStringFilter: (String) -> Boolean = { s -> s.all { c -> c.isLetter() } }
-    val nonEmptyStringFilter: (String) -> Boolean = { it.isNotEmpty() }
+    val wordsFilter = WordsFilter()
     return WordCounter(
-        wordsFilter = listOf(nonEmptyStringFilter, latinAlphabeticStringFilter),
+        wordsFilter = listOf(wordsFilter.nonEmptyStringFilter, wordsFilter.latinAlphabeticStringFilter),
         wordCountListener = wordCountListener
     )
 }
---
+++ src/main/kotlin/wordcounter/domain/wordsfiltering/WordsFilter.kt
@@ -0,0 +1,6 @@
+package wordcounter.domain.wordsfiltering
+
+class WordsFilter {
+    val latinAlphabeticStringFilter: (String) -> Boolean = { s -> s.all { c -> c.isLetter() } }
+    val nonEmptyStringFilter: (String) -> Boolean = { it.isNotEmpty() }
+}
