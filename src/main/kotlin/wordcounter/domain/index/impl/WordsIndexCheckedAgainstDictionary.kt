e82879df2d3702a3ffbb3c475525b625d93c88a0
Claus Polanka <claus.polanka@chello.at>
2025-04-17 21:29:59 +0200

    Reorder constructor arguments

--- src/main/kotlin/wordcounter/domain/index/impl/WordsIndexCheckedAgainstDictionary.kt
+++ src/main/kotlin/wordcounter/domain/index/impl/WordsIndexCheckedAgainstDictionary.kt
@@ -5,8 +5,8 @@ import wordcounter.domain.index.api.*
 private const val UNKNOWN_MARK = "*"
 
 class WordsIndexCheckedAgainstDictionary(
-    private val wordsIndexListener: WordsIndexListener,
     private val dictionary: List<String>,
+    private val wordsIndexListener: WordsIndexListener,
 ) : WordsIndexListener {
     override fun onWordsIndexed(words: List<String>) {
         val index = words.indexCheckedAgainst(dictionary)
