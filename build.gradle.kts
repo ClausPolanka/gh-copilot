fcabcf7ba240058a490947c7f25a28c17008c600
Claus Polanka <claus.polanka@chello.at>
2025-04-18 12:21:16 +0200

    Upgrade Kotlin to 2.2.0-Beta1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    id("org.jetbrains.kotlin.jvm") version "2.2.0-Beta1"
     id("com.github.ben-manes.versions") version "0.52.0"
     idea
 }
